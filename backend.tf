cae4583bd1d31f006021c9812009e2b315ac4000
thatireddy355 <thatireddy.v@gmail.com>
2025-06-27 15:39:43 +0530

    added provider configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-terraform-state-bucket-0355"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
